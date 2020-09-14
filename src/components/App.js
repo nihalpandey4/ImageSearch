11c9a5ae6c36dc05f40d94a145dc62a80e60f661
nihalpandey4 <61798365+nihalpandey4@users.noreply.github.com>
2020-09-15 03:52:14 +0530

    Loader added

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import unsplash from "../apiRequest/unsplash";
 import Searchbar from "./Searchbar";
 import ImagesList from "./ImagesList";
+import Loader from "./Loader";
 import "./components.css";
 
 class App extends React.Component{
@@ -13,11 +14,20 @@ class App extends React.Component{
     this.setState({images:response});
   }
 
+  renderResults=()=>{
+    if(this.state.images.length>0){
+      return <ImagesList images={this.state.images} />;
+    }
+    else{
+      return <Loader />
+    }
+  }
+
   render(){
     return (
       <div className="ui container">
         <Searchbar onSubmittingData={this.onSearch} />
-        <ImagesList images={this.state.images} />
+        {this.renderResults()}
       </div>
     );
   }
---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Loader =()=>{
+    return (
+        <div className="ui large active centered inline loader"></div>
+    )
+}
+export default Loader;
